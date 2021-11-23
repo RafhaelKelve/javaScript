37a1f261bec80ea6adf1d49d4314a036d56fee59
RafhaelKelve <82288130+RafhaelKelve@users.noreply.github.com>
2021-11-23 16:00:44 -0300

    inserção de código para verificar se é adolescente, adulto ou idoso

---
+++ condicao/ver-idade.js
@@ -0,0 +1,11 @@
+var idade = 51;
+
+if(idade < 18) {
+    console.log("Adolescente");
+} else if(idade >= 19 && idade <=50) {
+    console.log("Adulto");
+} else if(idade > 50) {
+    console.log("Idoso");
+} else {
+    console.log("Operacão Invalida!");
+}

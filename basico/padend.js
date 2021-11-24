bade82a411dc634540ab20b94a11bb294879f54e
RafhaelKelve <82288130+RafhaelKelve@users.noreply.github.com>
2021-11-24 18:22:42 -0300

    criei uma pasta com o nome basico e com arquivos e códigos neles.

---
+++ basico/padend.js
@@ -0,0 +1,4 @@
+let str = "9";
+str = str.padEnd(4,0);
+
+console.log(str);
---
+++ basico/values.js
@@ -0,0 +1,7 @@
+const fornecedor = {
+ razaosocial : "Kelve",
+ cnpj : "00.000.000/0000-00", 
+ email : "rafhaelkelve@hotmail.com"
+};
+
+console.log(Object.values(fornecedor));
